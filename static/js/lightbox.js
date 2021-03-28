5c1a32d7b280bc27353c4002d4ab52b55e5baf89
Sara Martín Alonso <65310800+sara-martin-alonso@users.noreply.github.com>
2021-03-28 15:03:11 +0200

    Update lightbox.js

--- static/js/lightbox.js
+++ static/js/lightbox.js
@@ -1,10 +1,10 @@
 
 const lightbox = document.querySelector('.lightbox')
-console.log('5')
+
 const lightboxImg = document.querySelector('.lightbox__img')
-console.log('6')
+
 const lightboxIcon = document.querySelector('.lightbox__icon')
-console.log('7')
+
 
 lightboxMini.forEach(function (cadaMini, i) {
     cadaMini.addEventListener('click', function () {
@@ -12,11 +12,9 @@ lightboxMini.forEach(function (cadaMini, i) {
         lightboxImg.src = this.src
     })
 })
-console.log('15')
 lightboxIcon.addEventListener('click', function () {
     lightbox.classList.remove('ver')
 })
-console.log('16')
 
 document.body.addEventListener('keydown', function (e) {
     console.log(e.key)
@@ -25,4 +23,3 @@ document.body.addEventListener('keydown', function (e) {
     }
     
 })
-console.log('17')
